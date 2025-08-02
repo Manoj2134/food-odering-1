e40a4e58094b508f30f482d94b64f910074dc15d
Manoj2134 <120081644+Manoj2134@users.noreply.github.com>
2025-08-02 17:26:38 +0530

    Add files via upload

---
+++ food-ordering-app/backend/src/main/java/com/example/foodorder/FoodOrderingAppApplication.java
@@ -0,0 +1,7 @@
+package com.example.foodorder;
+
+public class FoodOrderingAppApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello Food Ordering App");
+    }
+}
